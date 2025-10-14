2515bf51dd56de0b92a88c5c445ce48422691752
oscar-perezRevons <oscarsamuelperezcallizaya@gmail.com>
2025-10-14 09:36:55 -0400

    refactor: Reestructurar y renombrar los directorios del proyecto_2

--- cypress/e2e/sumador.cy.js
+++ cypress/e2e/sumador.cy.js
@@ -1,4 +1,4 @@
-describe("Sumador", () => {
+/*describe("Sumador", () => {
   it("Shows the amount of the addition to the user", () => {
     cy.visit("/");
     cy.get("#primer-numero").type(4);
@@ -6,4 +6,4 @@ describe("Sumador", () => {
     cy.get("#sumar-button").click();
     cy.get("#resultado-div").should("contain", "9");
   });
-});
+});*/
