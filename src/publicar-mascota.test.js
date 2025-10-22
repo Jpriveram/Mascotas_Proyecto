952cb5607e4a06dd43067ecb62441686d63e5de0
Ostin <ostinrivas15@gmail.com>
2025-10-22 01:32:58 -0400

    fix: Solucione el test de publicar-mascota

--- src/publicar-mascota.test.js
+++ src/publicar-mascota.test.js
@@ -2,12 +2,7 @@ import publicarMascota from './publicar-mascota';
 
 describe('publicarMascota ', () => {
   it('debería publicar una mascota', () => {
-    expect(publicarMascota({
-      nombre: 'Firulais',
-      raza: 'Salchicha',
-      edad: '6 meses',
-      especie: 'Perro',
-      foto: 'foto.png'
-    })).toEqual('Firulais, Salchicha, 6 meses, Perro, foto.png');   
+    expect(publicarMascota('Firulais','Salchicha','6 meses','Perro','foto.png'
+    )).toEqual('Firulais, Salchicha, 6 meses, Perro, foto.png');   
     });
 });
